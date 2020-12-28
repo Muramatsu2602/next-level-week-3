e97de0c227a5e0fe23104f9d22ccea2c1c8dde6a
Muramatsu2602 <pedromuramatsuc@gmail.com>
2020-12-28 17:16:29 -0300

    Day2: Understanding Requests + Imnsonia

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,5 +6,38 @@ import express from 'express';
 
 const app = express(); // helps us deal with REQ 
 
-app.listen(3333);
 
+/**
+ * REQUISICAO:
+ * - Front->Back
+ * - Dados, informacao, parametro
+ * RESPONSE:
+ * - Como o back vai confirmar/responder
+ */
+
+// Rota = conjunto
+// Recurso = usuario
+
+// Metodos HTTP = GET, POST, PUT, DELETE
+// semantica eh oq diferencia cada um
+    // GET --> buscar uma informacao 
+    // POST --> criando uma informacao nova (o navegador nao acessa)
+    // PUT --> editando uma informacao
+    // DELETE --> autoexplicativo 
+// Parametros 
+// - Query Params: https://localhost:3333/users?search=diago&age=28
+// - Route Params: https://localhost:3333/users/1  (identificar um recurso)
+// - Body: http: https://localhost:333/users/ ==> informacoes complexas
+
+app.get('/users', (request, response) => {
+    
+    console.log(request.query);
+    console.log(request.params);
+
+
+    return response.json({ message: 'Hello World' });
+});
+
+
+
+app.listen(3333);
