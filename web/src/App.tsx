2203ff8c1cc7d3558d742abe0abddf6a501c0919
Muramatsu2602 <pedromuramatsuc@gmail.com>
2020-12-27 22:51:55 -0300

    Day1: landing page css

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
 import './styles/global.css';
+import './styles/pages/landing.css';
 
-interface TitleProps{
+import logoimg from './images/logo.svg';
+
+interface TitleProps {
   text: string;
 }
 
@@ -14,11 +17,24 @@ function Title(props: TitleProps) {
 function App() {
   // JSX - Javascript XML --> crazy stuff!
   return (
-    <div className="App">
-      <Title text="Titulo 1" />
-      <Title text="Titulo 2"/>
-      <Title text="Titulo 3"/>
-      <Title text="Titulo 4"/>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoimg} alt="logo" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças</p>
+        </main>
+
+        <div className="location">
+          <strong>Agudos</strong>
+          <span>São Paulo</span>
+        </div>
+
+        <a href="" className="enter-app">
+          ≥
+        </a>
+      </div>
     </div>
   );
 }
