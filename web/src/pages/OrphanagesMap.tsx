02d50f6a335737ce98dd1549c98ead2c2c3bdf64
Muramatsu2602 <pedromuramatsuc@gmail.com>
2020-12-27 23:47:38 -0300

    Day1: styling OrphanageMap

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
 
-function OrphanagesMap(){
-    return(
-        <h1>Hello World</h1>
+import mapMarkerImg from '../images/map-marker.svg';
 
+import '../styles/pages/orphanages-map.css';
+
+function OrphanagesMap() {
+    return (
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy" />
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças esperando a sua visita </p>
+                </header>
+
+                <footer>
+                    <strong>Agudos</strong>
+                    <span>São Paulo</span>
+                </footer>
+            </aside>
+
+            <div></div>
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#FFF" />
+            </Link>
+        </div>
     );
 }
+
 export default OrphanagesMap;
