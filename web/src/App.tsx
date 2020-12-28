4385e96ec29ac2916815c1e5949ce5808389f507
Muramatsu2602 <pedromuramatsuc@gmail.com>
2020-12-27 23:28:37 -0300

    Day1: using Router/Switch

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,42 +1,14 @@
 import React from 'react';
-import { FiArrowRight } from 'react-icons/fi';
 
 import './styles/global.css';
-import './styles/pages/landing.css';
 
-import logoimg from './images/logo.svg';
+import Routes from './routes';
 
-interface TitleProps {
-  text: string;
-}
-
-// this is a component
-function Title(props: TitleProps) {
-  return <h1> {props.text} </h1>
-}
 
 function App() {
   // JSX - Javascript XML --> crazy stuff!
   return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoimg} alt="logo" />
-
-        <main>
-          <h1>Leve felicidade para o mundo</h1>
-          <p>Visite orfanatos e mude o dia de muitas crianças</p>
-        </main>
-
-        <div className="location">
-          <strong>Agudos</strong>
-          <span>São Paulo</span>
-        </div>
-
-        <a href="" className="enter-app">
-          <FiArrowRight size={26} color="rgba(0,0,0,0.6)"/>
-        </a>
-      </div>
-    </div>
+    <Routes />
   );
 }
 
