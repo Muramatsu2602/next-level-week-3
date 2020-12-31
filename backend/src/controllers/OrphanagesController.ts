cd881b5d9830f23182896803e68c994845903dcb
Muramatsu2602 <pedromuramatsuc@gmail.com>
2020-12-31 16:46:06 -0300

    Day2: Using MVC + Index function is DONE

---
+++ backend/src/controllers/OrphanagesController.ts
@@ -0,0 +1,46 @@
+import { Request, Response } from 'express';
+import { getRepository } from 'typeorm' // Repository owns the rules on how to data is created/altered/deleted
+
+import Orphanage from '../models/Orphanage';
+
+export default {
+
+    async index(request: Request, response: Response) {
+        const orphanagesRepository = getRepository(Orphanage);
+
+        const orphanages = await orphanagesRepository.find();
+
+        return response.json(orphanages);
+    },
+
+    async create(request: Request, response: Response) {
+
+        // deconstructing the JSON onto its fields
+        const {
+            name,
+            latitude,
+            longitude,
+            about,
+            instructions,
+            opening_hours,
+            open_on_weekends
+        } = request.body;
+
+        const orphanagesRepository = getRepository(Orphanage);
+
+        const orphanage = orphanagesRepository.create({
+            name,
+            latitude,
+            longitude,
+            about,
+            instructions,
+            opening_hours,
+            open_on_weekends
+        });
+
+        await orphanagesRepository.save(orphanage);
+
+        return response.status(201).json(orphanage); // 201 --> algo foi criado
+
+    }
+};
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,68 +1,14 @@
-import express, { response } from 'express';
-import { getRepository } from 'typeorm' // Repository owns the rules on how to data is created/altered/deleted
-
+import express from 'express';
 import './database/connection';
-import Orphanage from './models/Orphanage';
+import routes from './routes';
 
 const app = express(); // helps us deal with REQ 
 
 app.use(express.json());
+app.use(routes);
 
-
-/**
- * REQUISICAO:
- * - Front->Back
- * - Dados, informacao, parametro
- * RESPONSE:
- * - Como o back vai confirmar/responder
- */
-
-// Rota = conjunto
-// Recurso = usuario
-
-// Metodos HTTP = GET, POST, PUT, DELETE
-// semantica eh oq diferencia cada um
-// GET    --> buscar uma informacao 
-// POST   --> criando uma informacao nova (o navegador nao acessa)
-// PUT    --> editando uma informacao
-// DELETE --> autoexplicativo 
-// Parametros 
-// - Query Params: https://localhost:3333/users?search=diago&age=28
-// - Route Params: https://localhost:3333/users/1  (identificar um recurso)
-// - Body        : http: https://localhost:333/users/ ==> informacoes complexas
-
-app.post('/orphanages',async (request, response) => {
-
-    // deconstructing the JSON onto its fields
-    const {
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends
-    } = request.body;
-
-    const orphanagesRepository = getRepository(Orphanage);
-
-    const orphanage = orphanagesRepository.create({
-        name,
-        latitude,
-        longitude,
-        about,
-        instructions,
-        opening_hours,
-        open_on_weekends
-    });
-
-    await orphanagesRepository.save(orphanage);
-
-    return response.json({ message: 'Hello World' });
-})
 
 app.listen(3333);
-
 
 // driver nativo, Query builder, ORM --> niveis de abstracoes para manipular BD
 
