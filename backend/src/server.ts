6f6c0007701b347e6a35410f7944e41ac9da8066
Muramatsu2602 <pedromuramatsuc@gmail.com>
2021-01-01 22:47:21 -0300

    Day2: Using cors and setting Error Handlers

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,9 @@
 import express from 'express';
-import './database/connection';
 import path from 'path';
+import cors from 'cors';
+
+// import 'express-async-errors';
+import './database/connection';
 
 import routes from './routes';
 import errorHandler from './errors/handler';
@@ -8,6 +11,7 @@ import errorHandler from './errors/handler';
 
 const app = express(); // helps us deal with REQ 
 
+app.use(cors()); // todos os front ends acessam a nossa API
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
