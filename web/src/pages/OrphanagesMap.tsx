9a0b12aa09f297858a3c7e7dc23f67a2cb25d9eb
Muramatsu2602 <pedromuramatsuc@gmail.com>
2021-01-03 16:17:17 -0300

    Day3: fixing Map to MapContainer + new routes

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -49,7 +49,7 @@ function OrphanagesMap() {
 
                     <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup" >
                         Lar dos Herois
-                        <Link to="">
+                        <Link to="/orphanages/1">
                             <FiArrowRight size={20} color="#FFF" />
                         </Link>
                     </Popup>
@@ -57,7 +57,7 @@ function OrphanagesMap() {
             </MapContainer>
 
 
-            <Link to="" className="create-orphanage">
+            <Link to="/orphanages/create" className="create-orphanage">
                 <FiPlus size={32} color="#FFF" />
             </Link>
         </div>
