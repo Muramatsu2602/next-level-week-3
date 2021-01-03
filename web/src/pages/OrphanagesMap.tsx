04f3ebe55a535bba56f9ddf6ee78308b571a276b
Muramatsu2602 <pedromuramatsuc@gmail.com>
2021-01-02 23:17:51 -0300

    Day3: styling map with cool marker

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { FiPlus } from 'react-icons/fi';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { FiPlus, FiArrowRight } from 'react-icons/fi';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
+import Leaflet from 'leaflet';
 import 'leaflet/dist/leaflet.css'
 
 import mapMarkerImg from '../images/map-marker.svg';
 
 import '../styles/pages/orphanages-map.css';
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarkerImg,
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
 
 function OrphanagesMap() {
     return (
@@ -17,8 +25,8 @@ function OrphanagesMap() {
                     <img src={mapMarkerImg} alt="Happy" />
 
                     <h2>Escolha um orfanato no mapa</h2>
-                    <p>Muitas crianças esperando a sua visita </p>
                 </header>
+                <p>Muitas crianças esperando a sua visita </p>
 
                 <footer>
                     <strong>Agudos</strong>
@@ -29,11 +37,23 @@ function OrphanagesMap() {
             <MapContainer
                 center={[35.7101694, 139.810003]}
                 zoom={15}
-                style={{width: '100%', height: '100%'}}
+                style={{ width: '100%', height: '100%' }}
             >
                 {/* <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/> */}
-                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>
+                <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
 
+                <Marker
+                    icon={mapIcon}
+                    position={[35.7101694, 139.810003]}
+                >
+
+                    <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup" >
+                        Lar dos Herois
+                        <Link to="">
+                            <FiArrowRight size={20} color="#FFF" />
+                        </Link>
+                    </Popup>
+                </Marker>
             </MapContainer>
 
 
