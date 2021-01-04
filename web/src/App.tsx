2448d7399efd3b4ba863af4b0575ffca233217fb
Muramatsu2602 <pedromuramatsuc@gmail.com>
2021-01-04 17:13:50 -0300

    Day3: using axios and integrating front+back

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './styles/global.css';
+import 'leaflet/dist/leaflet.css';
 
 import Routes from './routes';
 
@@ -11,5 +12,6 @@ function App() {
     <Routes />
   );
 }
+
 
 export default App;
