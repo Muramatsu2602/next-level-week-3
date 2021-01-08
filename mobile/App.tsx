d00e8917042cffb5d8b978ab8dcad77e993710d3
Muramatsu2602 <pedromuramatsuc@gmail.com>
2021-01-08 16:26:31 -0300

    styling map page + neat tags

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
+
+import mapMarker from './src/images/map-marker.png'
 
 /**
  * View --> div
@@ -11,7 +14,33 @@ export default function App() {
   return (
     // no HTML tags!
     <View style={styles.container}>
-      <StatusBar style="auto" />
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: -27.2092052,
+          longitude: -49.6401092,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008,
+        }}
+      >
+        <Marker
+          icon={mapMarker}
+          calloutAnchor={{
+            x: 2.7,
+            y: 0.8,
+          }}
+          coordinate={{
+            latitude: -27.2092052,
+            longitude: -49.6401092,
+          }}
+        >
+          <Callout tooltip={true} onPress={() => { alert('oi') }}>
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>Lar das Donzelas</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </MapView>
     </View>
   );
 }
@@ -21,4 +50,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255,255,255,0.8)',
+    borderRadius: 16,
+    justifyContent: 'center',
+  },
+
+  calloutText: {
+    color: '#0089a5',
+    fontSize: 14,
+  }
 });
