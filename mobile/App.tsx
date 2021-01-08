d155ca3d89e9d37dfcd79fe619002ccb1720cf01
Muramatsu2602 <pedromuramatsuc@gmail.com>
2021-01-08 17:01:55 -0300

    dealing with routes

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
-import MapView, { Marker, Callout, PROVIDER_GOOGLE } from 'react-native-maps';
-import { Feather } from '@expo/vector-icons';
+
 import { useFonts } from 'expo-font';
 import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from '@expo-google-fonts/nunito';
 
-import mapMarker from './src/images/map-marker.png'
-
-/**
- * View --> div
- * Text --> se quiser estilizar, vai ser separadamente
- */
+import Routes from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -24,104 +17,7 @@ export default function App() {
   }
 
   return (
-    // no HTML tags!
-    <View style={styles.container}>
-      <MapView
-        style={styles.map}
-        initialRegion={{
-          latitude: -27.2092052,
-          longitude: -49.6401092,
-          latitudeDelta: 0.008,
-          longitudeDelta: 0.008,
-        }}
-      >
-        <Marker
-          icon={mapMarker}
-          calloutAnchor={{
-            x: 2.7,
-            y: 0.8,
-          }}
-          coordinate={{
-            latitude: -27.2092052,
-            longitude: -49.6401092,
-          }}
-        >
-          <Callout tooltip={true} onPress={() => { alert('oi') }}>
-            <View style={styles.calloutContainer}>
-              <Text style={styles.calloutText}>Lar das Donzelas</Text>
-            </View>
-          </Callout>
-        </Marker>
-      </MapView>
-
-      <View style={styles.footer}>
-        <Text style={styles.footerText}>2 orfanatos encontrados</Text>
-
-        <TouchableOpacity style={styles.createOrphanageButton} onPress={() => { }}>
-          <Feather name="plus" size={20} color="#FFF" />
-        </TouchableOpacity>
-      </View>
-    </View>
+    <Routes />
   );
 }
 
-// display: flex is already implicit
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-
-  map: {
-    width: Dimensions.get('window').width,
-    height: Dimensions.get('window').height + 40, // for some reason thuis dosen't get all the height....
-  },
-
-  calloutContainer: {
-    width: 160,
-    height: 46,
-    paddingHorizontal: 16,
-    backgroundColor: 'rgba(255,255,255,0.8)',
-    borderRadius: 16,
-    justifyContent: 'center',
-
-
-    elevation: 3,
-  },
-
-  calloutText: {
-    color: '#0089a5',
-    fontSize: 14,
-  },
-
-  footer: {
-    position: 'absolute',
-    left: 24,
-    right: 24,
-    bottom: 32,
-
-    backgroundColor: '#FFF',
-    borderRadius: 20,
-    height: 56,
-    paddingLeft: 24,
-
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-
-    elevation: 3,
-  },
-
-  footerText: {
-    color: '#8fa7b3'
-  },
-
-  createOrphanageButton: {
-    width: 56,
-    height: 56,
-    backgroundColor: '#15c3d6',
-    borderRadius: 20,
-
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-});
