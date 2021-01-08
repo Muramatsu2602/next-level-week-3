9bff6820371956a1c0f5b2d2b8aafa0903a2c196
Muramatsu2602 <pedromuramatsuc@gmail.com>
2021-01-08 15:47:35 -0300

    installing expo maps + new gitignore file for building sake

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,20 +2,23 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+/**
+ * View --> div
+ * Text --> se quiser estilizar, vai ser separadamente
+ */
+
 export default function App() {
   return (
+    // no HTML tags!
     <View style={styles.container}>
-      <Text>Hello, World!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+// display: flex is already implicit
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
